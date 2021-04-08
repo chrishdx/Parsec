a2a01d376c12c3a98528d1e268b7299566c1c55b
chrishdx <50511550+chrishdx@users.noreply.github.com>
2021-04-08 05:50:19 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,39 @@
+sudo sh -c "echo 'h264_freq=550' >> /boot/config.txt"
+sudo sh -c "echo 'over_voltage=2 ' >> /boot/config.txt"
+sudo sh -c "echo 'gpu_mem=256' >> /boot/config.txt"
+sudo sh -c "echo 'disable_splash 1' >> /boot/config.txt"
+sudo sh -c "echo 'app_lan_quality=1' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'app_wan_quality=1' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'vencoder_bitrate=3' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'client_vsync=0' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'encoder_fps=30' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'encoder_min_qp=8' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'client_immersive=1' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'client_overlay=0' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'network_try_lan=1' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'client_windowed=0' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo '#client_window_x=1280' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo '#client_window_y=720' >> /home/pi/.parsec/config.txt"
+sudo sh -c "echo 'client_audio_buffer=26000' >> /home/pi/.parsec/config.txt"
+
+
+sudo nano /boot/cmdline.txt
+
+sudo sh -c "echo 'usbhid.mousepoll=8' >> /boot/cmdline.txt"
+
+
+sudo apt install openvpn -y
+sudo systemctl enable openvpn
+sudo systemctl enable openvpn@client.service
+
+sudo nano /etc/default/openvpn
+
+AUTOSTART="all"
+
+sudo mv /etc/openvpn/*.ovpn /etc/openvpn/offsite-client.conf
+
+
+curl https://raw.githubusercontent.com/virtualhere/script/main/install_server | sudo sh
+
+
+sudo reboot now
